299468a53ff352ee7fd94ad3e06904092d61533c
Cayo Eduardo da Silva Neves <cayo.neves@lifeapps.com.br>
2023-07-27 15:25:30 -0300

    Integrando Sonarqube no Jenkins

--- jenkins-lab/provision.sh
+++ jenkins-lab/provision.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 yum install epel-release -y
-yum install wget git -y
+yum install wget git unzip -y
 sudo wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install java-11-openjdk-devel -y
@@ -25,3 +25,16 @@ systemctl daemon-reload
 systemctl restart docker
 
 usermod -aG docker jenkins
+
+# Instalação do Sonar Scanner
+
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.2.2472-linux.zip
+unzip sonar-scanner-cli-4.6.2.2472-linux.zip -d /opt/
+mv /opt/sonar-scanner-4.6.2.2472-linux /opt/sonar-scanner
+chown -R jenkins:jenkins /opt/sonar-scanner
+echo 'export PATH=$PATH:/opt/sonar-scanner/bin' | sudo tee -a /etc/profile
+
+# Instalação do NodeJS
+
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+sudo yum install nodejs -y
